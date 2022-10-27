6889cc86799ae6bd86cc84679c305a0ec9e12110
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2022-10-28 00:12:11 +0200

    feat: add api request from pokeapi package

--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -1,22 +1,24 @@
 <script setup>
 import PokemonTile from '../components/PokemonTile.vue';
-let pokemons = [
-    {
-        id: 1,
-        name: "test",
-    },
-    {
-        id: 2,
-        name: "test2",
-    }
-];
-</script>
+import { ref, watchEffect } from 'vue';
 
+const P = new Pokedex.Pokedex({protocol: 'https', cachedImages: true});
+const pokemons = ref(null);
+const page = ref(0);
+
+watchEffect(async () => {
+  const data = await P.getPokemonsList();
+  console.log(data);
+  pokemons.value = await data.results;
+})
+</script>
 <template>
   <main>
-    <PokemonTile v-for="pokemon in pokemons" 
+    <PokemonTile v-for="(pokemon, index) in pokemons.slice(page*50,(page+1)*50)" 
     :name=pokemon.name
-    :id=pokemon.id
+    :id=(index+1)+page*50
     />
+    <button :disabled="page === 0" @click="page--">Previous Page</button>
+    <button :disabled="page >= pokemons.length/50-1" @click="page++">Next Page</button>
   </main>
 </template>
