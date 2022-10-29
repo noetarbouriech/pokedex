90c32f3bb7e1a618e8561a97b8b762fa2aa62f29
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2022-10-30 00:51:02 +0200

    feat: Add pokemon search by name

--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -5,20 +5,30 @@ import { ref, watchEffect } from 'vue';
 const P = new Pokedex.Pokedex({protocol: 'https', cachedImages: true});
 const pokemons = ref([]);
 const page = ref(0);
+const search = ref('');
 
 watchEffect(async () => {
-  const data = await P.getPokemonsList();
+  const data = await P.getPokemonSpecies();
   console.log(data);
   pokemons.value = await data.results;
 })
+
+function searchPokemons(pkm) {
+  return pkm.filter(data => data.name.toLowerCase().startsWith(search.value.toLowerCase()));
+}
+
+function getPkmIndex(name) {
+  return pokemons.value.findIndex((i) => i.name === name) + 1;
+}
 </script>
 <template>
   <main>
-    <PokemonTile v-for="(pokemon, index) in pokemons.slice(page*50,(page+1)*50)" 
+    <input placeholder="Search" type="search" v-model="search">
+    <PokemonTile v-for="pokemon in searchPokemons(pokemons).slice(page*50,(page+1)*50)" 
     :name=pokemon.name
-    :id=(index+1)+page*50
+    :id=getPkmIndex(pokemon.name)
     />
     <button :disabled="page === 0" @click="page--">Previous Page</button>
-    <button :disabled="page >= pokemons.length/50-1" @click="page++">Next Page</button>
+    <button :disabled="page >= searchPokemons(pokemons).length/50-1" @click="page++">Next Page</button>
   </main>
 </template>
