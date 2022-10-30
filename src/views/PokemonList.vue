f6ceed4fc3aca8d8ed655d28a630bd575c665b9b
Noé Tarbouriech <noe.tarbouriech@etu.umontpellier.fr>
2022-10-30 23:24:24 +0100

    refactor: set pokemon id at fetch instead of using index

--- src/views/PokemonList.vue
+++ src/views/PokemonList.vue
@@ -9,24 +9,23 @@ const search = ref('');
 
 watchEffect(async () => {
   const data = await P.getPokemonSpecies();
-  console.log(data);
-  pokemons.value = await data.results;
+  pokemons.value = await data.results.map((pkm, id) => { 
+    pkm.id = id + 1;
+    return pkm;
+  });
 })
 
 function searchPokemons(pkm) {
   return pkm.filter(data => data.name.toLowerCase().startsWith(search.value.toLowerCase()));
 }
-
-function getPkmIndex(name) {
-  return pokemons.value.findIndex((i) => i.name === name) + 1;
-}
 </script>
+
 <template>
   <main>
     <input placeholder="Search" type="search" v-model="search">
     <PokemonTile v-for="pokemon in searchPokemons(pokemons).slice(page*50,(page+1)*50)" 
     :name=pokemon.name
-    :id=getPkmIndex(pokemon.name)
+    :id=pokemon.id
     />
     <button :disabled="page === 0" @click="page--">Previous Page</button>
     <button :disabled="page >= searchPokemons(pokemons).length/50-1" @click="page++">Next Page</button>
